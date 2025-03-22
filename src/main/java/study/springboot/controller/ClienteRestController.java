94c961cc32d3b7ac38fc5208434c3ba81ac07f12
Jhonata <jhonatafontesluis23@gmail.com>
2025-03-22 16:27:10 -0300

    Implementando a view do projeto usando Thymeleaf
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

--- src/main/java/study/springboot/controller/ClienteRestController.java
+++ src/main/java/study/springboot/controller/ClienteRestController.java
@@ -2,9 +2,11 @@ package study.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,14 +34,19 @@ public class ClienteRestController {
 
     @PostMapping
     public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente){
-
         clienteService.inserir(cliente);
         return ResponseEntity.ok(cliente);
     }
 
+    @PutMapping("/{id}")
     public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
-
         clienteService.atualizar(id, cliente);
         return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        clienteService.deletar(id);
+        return ResponseEntity.ok().build();
     }
 }
